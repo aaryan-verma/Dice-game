ba05519fdc88f65e78d972a713463c2cb44f2aba
aaryan-verma <57305650+aaryan-verma@users.noreply.github.com>
2020-06-02 20:20:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ var randomImageSource2="images/dice"+randomNumber2+".png";
 document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
 
 if(randomNumber1>randomNumber2){
-  document.querySelector("h1").innerHTML="Player 1 wins";
+  document.querySelector("h1").innerHTML="Armaegddon wins";
 }
 else if (randomNumber2>randomNumber1) {
-  document.querySelector("h1").innerHTML="Player 2 wins";
+  document.querySelector("h1").innerHTML="@ wins";
 }
 else{
   document.querySelector("h1").innerHTML="draw";
